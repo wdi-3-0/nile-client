fd434bac27aa4179818021138511a947f4c5dc02
Charlie <charliekelly31415@gmail.com>
2019-03-28 16:48:08 -0400

    Get product list on load

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,6 +6,8 @@
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
+const products = require('./products')
+
 $(() => {
-  // your JS code goes here
+  products.loadProducts()
 })
