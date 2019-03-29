790415e0dd0905285f354d88fe147fbccccb41c0
Charlie <charliekelly31415@gmail.com>
2019-03-29 15:13:38 -0400

    UI handlers for cart events

--- assets/scripts/purchases/ui.js
+++ assets/scripts/purchases/ui.js
@@ -1,12 +1,12 @@
 'use strict'
 
-// const utils = require('./utils')
 const toast = require('../templates/toast')
 const shoppingCartTmpl = require('../templates/cart.hbs')
 
 const cartSuccess = (responseData) => {
+  // console.log(responseData)
   const cartHtml = shoppingCartTmpl({ cart: responseData.cart })
-  $('#shopping-cart-form .cart-content').html(cartHtml)
+  $('#shopping-cart-modal .cart-contents').html(cartHtml)
   $('#shopping-cart-modal').modal('show')
 }
 
@@ -14,7 +14,29 @@ const cartFailure = (responseData) => {
   toast.failure('Unable to open cart')
 }
 
+const addItemSuccess = (responseData) => {
+  toast.success('Item added to cart')
+}
+
+const addItemFailure = (responseData) => {
+  toast.failure('Unable to add item')
+}
+
+const removeItemSuccess = (responseData) => {
+  toast.success('Item removed from cart')
+  $('.modal').modal('hide')
+  $('#nav-cart-button').trigger('click')
+}
+
+const removeItemFailure = (responseData) => {
+  toast.failure('Unable to remove item')
+}
+
 module.exports = {
   cartSuccess,
-  cartFailure
+  cartFailure,
+  addItemSuccess,
+  addItemFailure,
+  removeItemSuccess,
+  removeItemFailure
 }
