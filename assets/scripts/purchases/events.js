85529be90f282b1963d86e29458f1a72fa0664a6
Charlie <charliekelly31415@gmail.com>
2019-03-29 13:21:48 -0400

    Create handlers/behaviors for cart

--- assets/scripts/purchases/events.js
+++ assets/scripts/purchases/events.js
@@ -1,4 +1,28 @@
 'use strict'
 
+const api = require('./api')
+const ui = require('./ui')
+
+const addHandlers = () => {
+  $('#nav-cart-button').on('click', showCart)
+  $('.main').on('click', '.add-item button', addToCart)
+}
+
+const showCart = () => {
+  api.getCart()
+    .then(ui.cartSuccess)
+    .catch(ui.cartFailure)
+}
+
+const addToCart = (event) => {
+  const productId = $(event.target).data('id')
+  api.addItem(productId)
+    .then()
+    .catch()
+}
+
 module.exports = {
+  addHandlers,
+  showCart,
+  addToCart
 }
--- assets/scripts/purchases/ui.js
+++ assets/scripts/purchases/ui.js
@@ -1,4 +1,20 @@
 'use strict'
 
+// const utils = require('./utils')
+const toast = require('../templates/toast')
+const shoppingCartTmpl = require('../templates/cart.hbs')
+
+const cartSuccess = (responseData) => {
+  const cartHtml = shoppingCartTmpl({ cart: responseData.cart })
+  $('#shopping-cart-form .cart-content').html(cartHtml)
+  $('#shopping-cart-modal').modal('show')
+}
+
+const cartFailure = (responseData) => {
+  toast.failure('Unable to open cart')
+}
+
 module.exports = {
+  cartSuccess,
+  cartFailure
 }
