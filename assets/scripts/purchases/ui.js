23eb8954c65946b6f2b16b3b988b77bf28882516
Charlie <charliekelly31415@gmail.com>
2019-03-31 16:17:12 -0400

    Add new response handlers
    - History: show order history
    - Checkout: just a stub for now, add more when Stripe is working

--- assets/scripts/purchases/ui.js
+++ assets/scripts/purchases/ui.js
@@ -2,6 +2,7 @@
 
 const toast = require('../templates/toast')
 const shoppingCartTmpl = require('../templates/cart.hbs')
+const historyTmpl = require('../templates/history.hbs')
 
 const cartSuccess = (responseData) => {
   // console.log(responseData)
@@ -32,11 +33,34 @@ const removeItemFailure = (responseData) => {
   toast.failure('Unable to remove item')
 }
 
+const historySuccess = (responseData) => {
+  const historyHtml = historyTmpl({ purchases: responseData.purchases })
+  $('#orders-modal .order-content').html = historyHtml
+  $('#orders-modal').modal('show')
+}
+
+const historyFailure = (responseData) => {
+  toast.failure('Unable to show purchase history')
+}
+
+const checkoutSuccess = (responseData) => {
+  // TODO: process order
+  toast.success('Checkout successful!')
+}
+
+const checkoutFailure = (responseData) => {
+  toast.failure('Unable to process order')
+}
+
 module.exports = {
   cartSuccess,
   cartFailure,
   addItemSuccess,
   addItemFailure,
   removeItemSuccess,
-  removeItemFailure
+  removeItemFailure,
+  historySuccess,
+  historyFailure,
+  checkoutSuccess,
+  checkoutFailure
 }
