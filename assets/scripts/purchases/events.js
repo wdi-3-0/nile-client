0aa07b76970944cb8e373e8626bd3e8b67db663f
Charlie <charliekelly31415@gmail.com>
2019-03-31 15:28:29 -0400

    Handle addToCart action
    - handles the addToCart event in all 3 situations:
    1. User not logged in -> open login modal
    2. User logged in but has no cart -> create cart, add item
    3. User logged in, has cart -> add item

--- assets/scripts/purchases/events.js
+++ assets/scripts/purchases/events.js
@@ -1,12 +1,26 @@
 'use strict'
 
+const utils = require('../utils')
 const api = require('./api')
 const ui = require('./ui')
 
 const addHandlers = () => {
   $('#nav-cart-button').on('click', showCart)
-  $('.main').on('click', '.add-item button', addToCart)
+  $('.main').on('click', '.add-item button', onClickAdd)
   $('#shopping-cart-modal').on('click', '.remove-item', removeFromCart)
+}
+
+// runs when user clicks 'Add to Cart' button, whether
+// or not they are logged in
+const onClickAdd = (event) => {
+  // check if user is logged in
+  if (utils.isAuthenticated()) {
+    // if logged in, add item to cart
+    addToCart(event)
+  } else {
+    // if not, show login dialog
+    $('#sign-in-modal').modal('show')
+  }
 }
 
 const showCart = () => {
@@ -17,9 +31,22 @@ const showCart = () => {
 
 const addToCart = (event) => {
   const productId = $(event.target).data('id')
-  api.addItem(productId)
-    .then(ui.addItemSuccess)
-    .catch(ui.addItemFailure)
+
+  // check if user has cart
+  if (!api.hasCart()) {
+    // if not, create cart
+    api.createCart()
+      .then(() => {
+        api.addItem(productId)
+          .then(ui.addItemSuccess)
+          .catch(ui.addItemFailure)
+      })
+      .catch(ui.addItemFailure)
+  } else {
+    api.addItem(productId)
+      .then(ui.addItemSuccess)
+      .catch(ui.addItemFailure)
+  }
 }
 
 const removeFromCart = (event) => {
