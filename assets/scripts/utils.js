98b8b200e9253de8115276651ee11adad7db35a8
Charlie <charliekelly31415@gmail.com>
2019-04-01 15:10:26 -0400

    Order total function

--- assets/scripts/utils.js
+++ assets/scripts/utils.js
@@ -13,7 +13,17 @@ const getCurrentUserId = () => {
   return store.user.id
 }
 
+const calculateOrderTotal = (order, orderProperty = 'totalCost') => {
+  const prices = order.items.map(item => item.price)
+  const total = prices.reduce((acc, val) => acc + val)
+  if (orderProperty !== '') {
+    order[orderProperty] = total
+  }
+  return total
+}
+
 module.exports = {
   isAuthenticated,
-  getCurrentUserId
+  getCurrentUserId,
+  calculateOrderTotal
 }
