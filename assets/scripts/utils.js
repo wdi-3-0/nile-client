d4a246e14b98025a8d453230e58e3b76bed233b7
Charlie <charliekelly31415@gmail.com>
2019-04-01 17:01:14 -0400

    Fix date format

--- assets/scripts/utils.js
+++ assets/scripts/utils.js
@@ -24,7 +24,7 @@ const calculateOrderTotal = (order, orderProperty = 'totalCost') => {
 
 const formatDate = (date) => {
   const day = date.getDate()
-  const month = date.getMonth()
+  const month = date.getMonth() + 1
   const year = date.getFullYear()
   return `${month}/${day}/${year}`
 }
