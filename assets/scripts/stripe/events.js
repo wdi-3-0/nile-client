3587cfa6973698cf0f2a9778d7b4be74ce3cb3aa
TaraYoo <yootar@gmail.com>
2019-04-01 11:15:31 -0400

    Pass in cart total to stripe checkout form
    Add a get cart ajax request to directly receive cart total from the
    back end

--- assets/scripts/stripe/events.js
+++ assets/scripts/stripe/events.js
@@ -1,37 +1,60 @@
 'use strict'
 
 const api = require('./api')
+const cartApi = require('../purchases/api')
 
 const addHandlers = () => {
   $('#checkout-button').on('click', openHandler)
 }
+
+// const getCartTotal = () => {
+//   cartApi.getCart()
+//     .then(responseData => {
+//       console.log(responseData)
+//       return responseData
+//     })
+// }
 
 const handler = StripeCheckout.configure({
   key: 'pk_test_IlHFjvgJoAsnL6ec5Nnbgxew005EW6mYqU',
   image: 'https://stripe.com/img/documentation/checkout/marketplace.png',
   locale: 'auto',
   token: function (token) {
-    const stripeData = {
-      token: {
-        tokenId: token.id,
-        total: 400
-      }
-    }
+    cartApi.getCart()
+      .then(responseData => {
+        const prices = responseData.cart.items.map(item => item.price)
+        const reducer = (acc, curr) => acc + curr
+        const total = prices.reduce(reducer) * 100
 
-    api.createCharge(stripeData)
-      .then(console.log)
-      .catch(console.error)
+        const stripeData = {
+          token: {
+            tokenId: token.id,
+            total
+          }
+        }
+
+        api.createCharge(stripeData)
+          .catch(console.error)
+      })
   }
 })
 
 const openHandler = event => {
   event.preventDefault()
 
-  handler.open({
-    name: 'Nile',
-    description: '2 widgets',
-    amount: 2000
-  })
+  cartApi.getCart()
+    .then(responseData => {
+      const prices = responseData.cart.items.map(item => item.price)
+      const reducer = (acc, curr) => acc + curr
+      const total = prices.reduce(reducer) * 100
+      console.log(total)
+
+      handler.open({
+        name: 'Nile',
+        description: '2 widgets',
+        amount: total
+      })
+    })
 }
 
 module.exports = {
